b5a98a1a404b104225552ad30facc401ce84f6b7
tgordon62 <tmangordo@gmail.com>
2023-07-09 09:36:55 -0500

    Extended tests array (expected results)

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -57,6 +57,10 @@ func TestNextToken(t *testing.T) {
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
 		{token.EOF, ""},
 	}
 
