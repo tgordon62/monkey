0ccdc8797f519717e9fc30f9b752793413413e39
tgordon62 <tmangordo@gmail.com>
2023-07-09 09:02:15 -0500

    Added isDigit() function

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -79,6 +79,10 @@ func isLetter(ch byte) bool {
 		ch == '_'
 }
 
+func isDigit(ch byte) bool {
+	return '0' >= ch && '9' <= ch
+}
+
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
