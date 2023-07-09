1108821e1e58f87e1f789c83a2da28b3d9e0228e
tgordon62 <tmangordo@gmail.com>
2023-07-09 08:05:00 -0500

    Base monkey tokens created.

---
+++ token/token.go
@@ -0,0 +1,32 @@
+package token
+
+type TokenType string
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers and literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCITON = "FUNCTION"
+	LET      = "LET"
+)
