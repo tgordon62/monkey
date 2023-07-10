c91c84f03be26195f13fcc6e93f935acb33658b1
tgordon62 <tmangordo@gmail.com>
2023-07-10 13:29:57 -0500

    Tests updated to include <, >, !, - , /

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -8,13 +8,15 @@ import (
 func TestNextToken(t *testing.T) {
 	input := `
     let five = 5;
-    let ten = 10;
-    let add = fn(x, y) {
-      x+y;
-    };
-    let result = add(five, ten);
-    !-/*5;
-    5 < 10 > 5;
+		let ten = 10;
+
+		let add = fn(x,y) {
+			x + y;
+		};
+
+		let result = add(five, ten);
+		!-/*5;
+		5 < 10 > 5;
   `
 
 	tests := []struct {
@@ -61,6 +63,14 @@ func TestNextToken(t *testing.T) {
 		{token.MINUS, "-"},
 		{token.SLASH, "/"},
 		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
