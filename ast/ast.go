3b8a3ca0cd55e547ce7d4598e0afdc6ca73d6446
tgordon62 <tmangordo@gmail.com>
2023-07-11 17:07:32 -0500

    Added LetStatement and Identifier structs

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "monkey-interpreter/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -25,3 +27,20 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 }
+
+type LetStatement struct {
+	Token token.Token // Associated token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // Associated token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
