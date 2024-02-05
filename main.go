de5ee53df61084fd58a9fb4300fdda1b00e79db4
tgordon62 <tmangordo@gmail.com>
2024-02-05 17:12:31 -0600

    File io option implemented.

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"monkey-interpreter/evaluator"
+	"monkey-interpreter/lexer"
+	"monkey-interpreter/object"
+	"monkey-interpreter/parser"
 	"monkey-interpreter/repl"
 	"os"
 	"os/user"
@@ -12,7 +17,63 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	if len(os.Args) == 1 {
+		readEvalPrintLoop(user)
+	} else {
+		fileArgument(user)
+	}
+
+}
+
+func readEvalPrintLoop(user *user.User) {
 	fmt.Printf("Hello %s! This is the Monkey language console!\n", user.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
+}
+
+func fileArgument(user *user.User) {
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	stat, err := f.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	buf := make([]byte, stat.Size())
+
+	_, err = f.Read(buf)
+	if err != nil {
+		panic(err)
+	}
+
+	str := string(buf)
+	l := lexer.New(str)
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+	if len(p.Errors()) != 0 {
+		printParserErrors(os.Stderr, p.Errors())
+		os.Exit(1)
+	}
+
+	env := object.NewEnvironment()
+
+	evaluated := evaluator.Eval(program, env)
+	if evaluated != nil {
+		io.WriteString(os.Stdout, evaluated.Inspect())
+		io.WriteString(os.Stdout, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Whoops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
 }
